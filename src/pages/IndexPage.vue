4da46bf0dfb87a2d83c4080a401a021fe57f464a
Brntpcnr <brntpcnr@gmail.com>
2023-03-23 13:42:48 +0300

    Getting closer to the to-do list

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page padding>
     <ul>
-      <li v-for="(task, index) in tasks"> <!-- index makes it so that for example we put a delete button next to each task, with index we are able to assign a button to that task -->
-        <div> {{ task.name }} {{ index }}</div>
-        <small> {{ task.dueDate }} @ {{ task.dueTime }}</small>
-        <button @click="deleteTask(index)">X</button>
-      </li>
-    </ul>
+      <task
+      v-for="(task, index) in tasks"
+      :key="task.id"
+      :task="task"
+      :index="index">{{ task.name }}</task>           <!-- to use the task component we imported we create an html element, now we need to pass tasks from the index.vue(page component) into the task(child component)   -->
+    </ul>                              <!-- to pass data from parent component to child component we will use props, :task="" is our prop in this case -->
   </q-page>
 </template>
 
@@ -16,15 +16,19 @@ export default {
       return {
         tasks:[
           {
+            id: 1,
             name:'Go to shop',
             dueDate: '12/04/2023',
             dueTime: '18:30'
           },
           {
+            id: 2,
             name:'Get bananas',
             dueDate: '13/04/2023',
             dueTime: '16:30'
-          },{
+          },
+          {
+            id: 3,
             name:'Get apples',
             dueDate: '13/04/2023',
             dueTime: '17:00'
@@ -34,8 +38,11 @@ export default {
     },
     methods :{
       deleteTask(index) {
-        this.tasks.splice(index, 1) //splice basically means delete, 1 means that one task at a time will be deleted
+        this.tasks.splice(index, 1)  //splice basically means delete, 1 means that one task at a time will be deleted
       }
+    },
+    components: {   //quick way to import from another page, in this case we are importing child components. We specify the name of the component as task, as it can be seen below
+      'task' : require('components/Task.vue').default
     }
  }
 </script>
