ca7b4b1dedcf445f5b6415c50d712091a4082929
Brntpcnr <brntpcnr@gmail.com>
2023-03-24 14:19:12 +0300

    To-Do List

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,124 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Todo</div>
+        <div class="text-subtitle1">{{todaysDate}}</div> <!-- computed method la aldığımız dinamik bilgiyi buraya yazıyoruz -->
+
+      </div>
+      <q-img
+        src="assets/mountain.jpg"
+        class="header-image absolute-top"
+      />
+    </q-header>
+
+    <q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="200"
+        :breakpoint="400"
+      >
+        <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="inbox" />
+              </q-item-section>
+
+              <q-item-section>
+                Inbox
+              </q-item-section>
+            </q-item>
+
+            <q-item active clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="star" />
+              </q-item-section>
+
+              <q-item-section>
+                Star
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="send" />
+              </q-item-section>
+
+              <q-item-section>
+                Send
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="drafts" />
+              </q-item-section>
+
+              <q-item-section>
+                Drafts
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="assets/baran.jpeg">
+            </q-avatar>
+            <div class="text-weight-bold">Baran Topçınar</div>
+            <div>@Brntpcnr</div>
+          </div>
+        </q-img>
+      </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+import {date} from 'quasar'
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+export default ({
+  name: 'MyLayout',
+
+  data () {
+    return{
+      leftDrawerOpen: false
+    }
+  },
+  computed: {
+    todaysDate() {
+      const timeStamp = Date.now()
+      return date.formatDate(timeStamp, 'dddd D MMMM')
+    }
+  }
+
+})
+</script>
+
+<style lang="scss">
+  .header-image {
+    height: 100%; //so it doesnt overflow its container
+    z-index: -1; //so that the background image stays behind other elements
+    opacity: 0.2;
+    filter: grayscale(100%)
+  }
+</style>
